9c2a3aead4b1d7eccebd0fe5c4c4704b99916287
Oswin <guiy24@mails.tsinghua.edu.cn>
2025-09-12 18:06:04 +0800

    初始提交

---
+++ brainlgn_x/__init__.py
@@ -0,0 +1,9 @@
+"""
+BrainLGN-X package skeleton.
+
+This package provides a BrainState/BrainPy-based reimplementation of an LGN
+filter network pipeline compatible with existing BMTK FilterNet configs.
+
+Current status: skeleton only. Fill core modules to enable functionality.
+"""
+
