5d0a1d6c4ef734b2bd718f81aa955629207658ff
Oswin <guiy24@mails.tsinghua.edu.cn>
2025-09-13 13:47:22 +0800

    更新包标识

--- brainlgn_x/__init__.py
+++ brainlgn_x/__init__.py
@@ -5,5 +5,11 @@ This package provides a BrainState/BrainPy-based reimplementation of an LGN
 filter network pipeline compatible with existing BMTK FilterNet configs.
 
 Current status: skeleton only. Fill core modules to enable functionality.
+
+
+@author: gray
+@date: 2025-09-12
+@e-mail: oswin0001@qq.com
+@Liu-Lab
 """
 
